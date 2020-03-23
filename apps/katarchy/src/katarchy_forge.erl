06619013ac258c4bc42707b4ad7dda1210df56b6
Marcelino Alberdi Pereira <marcelino.alberdi@gmail.com>
2020-03-23 23:34:30 +0100

    Adds forge return values when the blueprints are not applicable

--- apps/katarchy/src/katarchy_forge.erl
+++ apps/katarchy/src/katarchy_forge.erl
@@ -18,28 +18,42 @@ options(Mech, BPs) ->
 %% Internal functions
 %%--------------------------------------------------------------------
 apply_mod({attack_power, Fun, Value}, Mech) ->
-  NewValue = apply_mod_fun(Fun, Value, Mech#mech.attack_power),
+  NewValue = apply_mod_fun(Fun, Value, Mech#mech.attack_power, attack_power),
   Mech#mech{attack_power = NewValue};
 apply_mod({hit_points, Fun, Value}, Mech) ->
-  NewValue = apply_mod_fun(Fun, Value, Mech#mech.hit_points),
+  NewValue = apply_mod_fun(Fun, Value, Mech#mech.hit_points, hit_points),
   Mech#mech{hit_points = NewValue};
 apply_mod({speed, Fun, Value}, Mech) ->
-  NewValue = apply_mod_fun(Fun, Value, Mech#mech.speed),
+  NewValue = apply_mod_fun(Fun, Value, Mech#mech.speed, speed),
   Mech#mech{speed = NewValue}.
 
 
-apply_mod_fun(minus, Param, OldValue) ->
-  OldValue - Param;
-apply_mod_fun(plus, Param, OldValue) ->
+apply_mod_fun(minus, Param, OldValue, hit_points) ->
+  case Param >= OldValue of
+    true -> throw({not_applicable, {hit_points, gte, Param+1}});
+    false -> OldValue - Param
+  end;
+apply_mod_fun(minus, Param, OldValue, Field) ->
+  case Param > OldValue of
+    true -> throw({not_applicable, {Field, gte, Param}});
+    false -> OldValue - Param
+  end;
+apply_mod_fun(plus, Param, OldValue, _) ->
   OldValue + Param;
-apply_mod_fun(set, Param, _) ->
+apply_mod_fun(set, Param, _, _) ->
   Param.
 
 
 options(_, [], Options) ->
   lists:reverse(Options);
 options(Mech, [BP|BPs], Options) ->
-  NewMech = lists:foldl(fun(Mod, MechAcc) -> apply_mod(Mod, MechAcc) end,
-                        Mech, BP#blueprint.mods),
-  options(Mech, BPs, [{NewMech,BP}|Options]).
+  FoldFun = fun(Mod, {NewMech, Errors}) ->
+                try {apply_mod(Mod, NewMech), Errors}
+                catch throw:{not_applicable, Reason} ->
+                        {NewMech, [Reason|Errors]}
+                end end,
+  case lists:foldl(FoldFun, {Mech, []}, BP#blueprint.mods) of
+    {NewMech, []} -> options(Mech, BPs, [{NewMech,BP}|Options]);
+    {_, Errors} -> options(Mech, BPs, [{not_applicable, Errors, BP}|Options])
+  end.
 
--- apps/katarchy/test/katarchy_forge_SUITE.erl
+++ apps/katarchy/test/katarchy_forge_SUITE.erl
@@ -14,18 +14,26 @@ suite() ->
 
 all() ->
   [attack_power_decrease,
+   attack_power_decrease_not_applicable,
+   attack_power_decrease_to_zero,
    attack_power_increase,
    attack_power_reset,
    attack_power_set,
    empty_blueprints,
    hitpoints_decrease,
+   hitpoints_decrease_not_applicable,
+   hitpoints_decrease_to_zero,
    hitpoints_increase,
    hitpoints_set,
    multiple_blueprints,
    multiple_mods,
+   multiple_mods_decrease_not_applicable,
+   multiple_mods_decrease_one_not_applicable,
    same_mech,
    same_mech_modified,
    speed_decrease,
+   speed_decrease_not_applicable,
+   speed_decrease_to_zero,
    speed_increase,
    speed_set].
 
@@ -37,6 +45,18 @@ attack_power_decrease(_Config) ->
   BP = #blueprint{mods = [{attack_power, minus, 5}]},
   [{Mech, BP}] = katarchy_forge:options(#mech{attack_power = 8}, [BP]),
   3 = Mech#mech.attack_power.
+
+%% Test that a blueprint that decreases the power below 0 isn't applicable.
+attack_power_decrease_not_applicable(_Config) ->
+  BP = #blueprint{mods = [{attack_power, minus, 2}]},
+  [{not_applicable, [{attack_power, gte, 2}], BP}] =
+    katarchy_forge:options(#mech{}, [BP]).
+
+%% Test that a blueprint might decrease the attack power to zero.
+attack_power_decrease_to_zero(_Config) ->
+  BP = #blueprint{mods = [{attack_power, minus, 5}]},
+  [{Mech, BP}] = katarchy_forge:options(#mech{attack_power = 5}, [BP]),
+  0 = Mech#mech.attack_power.
 
 %% Test that a blueprint might increase the attack power.
 attack_power_increase(_Config) ->
@@ -67,6 +87,18 @@ hitpoints_decrease(_Config) ->
   [{Mech, BP}] = katarchy_forge:options(#mech{}, [BP]),
   2 = Mech#mech.hit_points.
 
+%% Test that a blueprint that decreases the hit poitns below 0 isn't applicable.
+hitpoints_decrease_not_applicable(_Config) ->
+  BP = #blueprint{mods = [{hit_points, minus, 14}]},
+  [{not_applicable, [{hit_points, gte, 15}], BP}] =
+    katarchy_forge:options(#mech{}, [BP]).
+
+%% Test that a blueprint that decreases the hit poitns to 0 isn't applicable.
+hitpoints_decrease_to_zero(_Config) ->
+  BP = #blueprint{mods = [{hit_points, minus, 10}]},
+  [{not_applicable, [{hit_points, gte, 11}], BP}] =
+    katarchy_forge:options(#mech{}, [BP]).
+
 %% Test that a blueprint might increase the hitpoints.
 hitpoints_increase(_Config) ->
   BP = #blueprint{mods = [{hit_points, plus, 5}]},
@@ -94,6 +126,18 @@ multiple_mods(_Config) ->
   15 = Mech#mech.hit_points,
   4 = Mech#mech.speed.
 
+%% Test that a single blueprint might have many not applicable decreases.
+multiple_mods_decrease_not_applicable(_Config) ->
+  BP = #blueprint{mods = [{attack_power, minus, 15}, {speed, minus, 4}]},
+  [{not_applicable, [{speed, gte, 4}, {attack_power, gte, 15}], BP}] =
+    katarchy_forge:options(#mech{}, [BP]).
+
+%% Test that a single blueprint might have a not applicable decrease.
+multiple_mods_decrease_one_not_applicable(_Config) ->
+  BP = #blueprint{mods = [{hit_points, plus, 5}, {speed, minus, 4}]},
+  [{not_applicable, [{speed, gte, 4}], BP}] =
+    katarchy_forge:options(#mech{}, [BP]).
+
 %% Test that the returned mech is the input if there aren't any mods.
 same_mech(_Config) ->
   BP = #blueprint{},
@@ -109,6 +153,18 @@ same_mech_modified(_Config) ->
 
 %% Test that a blueprint might decrease the speed.
 speed_decrease(_Config) ->
+  BP = #blueprint{mods = [{speed, minus, 2}]},
+  [{Mech, BP}] = katarchy_forge:options(#mech{speed = 9}, [BP]),
+  7 = Mech#mech.speed.
+
+%% Test that a blueprint that decreases the speed below 0 isn't applicable.
+speed_decrease_not_applicable(_Config) ->
+  BP = #blueprint{mods = [{speed, minus, 2}]},
+  [{not_applicable, [{speed, gte, 2}], BP}] =
+    katarchy_forge:options(#mech{speed = 1}, [BP]).
+
+%% Test that a blueprint might decrease the speed to zero.
+speed_decrease_to_zero(_Config) ->
   BP = #blueprint{mods = [{speed, minus, 2}]},
   [{Mech, BP}] = katarchy_forge:options(#mech{speed = 2}, [BP]),
   0 = Mech#mech.speed.
